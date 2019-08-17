7beca272b6c67f6f87bbafa07762f22bf4118021
liuhaoqing <liuhaoqing@dongjiang.com.cn>
2019-08-17 15:08:42 +0800

    修复分页数据库为null时不返回问题

--- system-common/src/main/java/com/liuhq/system/common/page/PageUtils.java
+++ system-common/src/main/java/com/liuhq/system/common/page/PageUtils.java
@@ -2,6 +2,7 @@ package com.liuhq.system.common.page;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.github.pagehelper.PageInfo;
 import com.google.common.collect.Lists;
 
@@ -18,8 +19,9 @@ public class PageUtils<T> {
 				.size(pageInfo.getSize()).startRow(pageInfo.getStartRow()).endRow(pageInfo.getEndRow())
 				.total(pageInfo.getTotal()).pages(pageInfo.getPages())
 				.list(Lists.<E, T>transform(pageInfo.getList(), obj -> {
-					return JSON.parseObject(JSON.toJSONString(obj), new TypeReference<T>() {
-					});
+					return JSON.parseObject(JSON.toJSONString(obj, SerializerFeature.WriteMapNullValue),
+							new TypeReference<T>() {
+							});
 				})).isFirstPage(pageInfo.isIsFirstPage()).isLastPage(pageInfo.isIsLastPage())
 				.hasPreviousPage(pageInfo.isHasPreviousPage()).hasNextPage(pageInfo.isHasNextPage())
 				.navigatePages(pageInfo.getNavigatePages()).navigatepageNums(pageInfo.getNavigatepageNums())
