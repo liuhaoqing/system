e169f28475b351301af2ffb52a70e29fa0964089
liuhaoqing <liuhaoqing@dongjiang.com.cn>
2019-08-17 14:23:44 +0800

    修改工具类

--- system-common/src/main/java/com/liuhq/system/common/utils/JsonUtils.java
+++ system-common/src/main/java/com/liuhq/system/common/utils/JsonUtils.java
@@ -2,21 +2,21 @@ package com.liuhq.system.common.utils;
 
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 public class JsonUtils {
 
 	@SuppressWarnings("unchecked")
 	public static Object getValueByKey(String json, String key) {
-		Map<String, Object> map = JSONObject.parseObject(json, Map.class);
-		String[] keys = key.split("\\.");
-		for (int i = 0; i < keys.length;) {
-			String k = keys[i];
-			return i != keys.length - 1
-					? getValueByKey(JSONObject.toJSONString(map.get(k)), key.substring(k.length() + 1))
+		if (JSON.isValidObject(json)) {
+			Map<String, Object> map = JSONObject.parseObject(json, Map.class);
+			String[] keys = key.split("\\.");
+			String k = keys[0];
+			return keys.length > 1 ? getValueByKey(JSONObject.toJSONString(map.get(k)), key.substring(k.length() + 1))
 					: map.get(k);
 		}
-		return null;
+		return json;
 	}
 
 }
